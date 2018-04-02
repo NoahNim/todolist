cc96c6a0a4d5552d2c7a868c6430fe3a6b5b3349
Alex Noble and Noah Medoff <noah@>
2018-04-02 11:46:13 -0700

    create todolist object and functionality to print that on the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
-var taskNumber=0;
-
-
+function toDoList(day, morningTask, lateMorningTask, afternoonTask, lateAfternoonTask, eveningTask){
+  this.day = day;
+  this.morningTask = morningTask;
+  this.lateMorningTask = lateMorningTask;
+  this.afternoonTask = afternoonTask;
+  this.lateAfternoonTask = lateAfternoonTask;
+  this.eveningTask = eveningTask;
+}
 
 $(document).ready(function() {
   $("form#toDoForm").submit(function(event) {
     event.preventDefault();
 
-    var inputtedTask = $("input#newTask").val();
+    var inputtedDay = $("input#day").val();
+    var inputtedMorning = $("input#morning").val();
+    var inputtedLateMorning = $("input#lateMorning").val();
+    var inputtedAfternoon = $("input#afternoon").val();
+    var inputtedLateAfternoon = $("input#lateAfternoon").val();
+    var inputtedEvening = $("input#eveningt").val();
+    var newTaskList = new toDoList(inputtedDay, inputtedMorning, inputtedLateMorning, inputtedAfternoon, inputtedLateAfternoon, inputtedEvening);
 
-    $("ul#taskList").append("<li id='task"+taskNumber+"'>" + inputtedTask +"</li>");
+    $("ul").append("<li>" + newTaskList.day + "</li><li>" + newTaskList.morningTask + "</li><li>" + newTaskList.lateMorningTask + "</li><li>" + newTaskList.afternoonTask + "</li><li>" + newTaskList.lateAfternoonTask + "</li><li>" + newTaskList.eveningTask + "</li>")
+    $("ul#taskList li").click(function(){
+      $(this).remove();
+    });
   });
 });
